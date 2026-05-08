e40600a086d40c5244c28f03d29b339f748657bf
lim444 <salimakbar21x@gmail.com>
2026-05-08 16:12:02 +0700

    create route create, edit, update and delete

--- Tugas 1/laravel/resources/views/jadwal/index.blade.php
+++ Tugas 1/laravel/resources/views/jadwal/index.blade.php
@@ -25,7 +25,7 @@
         <tbody>
             @forelse($jadwals as $j)
             <tr class="hover:bg-gray-50">
-                <td class="border px-4 py-2">{{ $loop->iteration }}</td>
+                <td class="border px-4 py-2">{{ $j->id }}</td>
                 <td class="border px-4 py-2">{{ $j->matakuliah->nama_matakuliah ?? '-' }}</td>
                 <td class="border px-4 py-2">{{ $j->dosen->nama ?? '-' }}</td>
                 <td class="border px-4 py-2">{{ $j->kelas }}</td>
